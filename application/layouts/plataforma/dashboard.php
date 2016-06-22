5eb28c18e9a0c881ef9d0b75f377f1f59ec0bef3
magnoalves34 <magno3344@icloud.com>
2016-06-22 12:53:43 -0300

    estrutura de arquivos e pastas

---
+++ application/layouts/plataforma/dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Painel</title>
+    </head>
+    <body>
+        <!-- topo -->
+
+       
+        <!-- /topo -->        
+        <!-- Sidebar -->
+
+       
+        <!-- /sidebar -->        
+        <!-- conteudo da pagina nao apgagar -->
+        {conteudo_pagina}
+        <!-- /conteudo da pagina -->        
+        <!-- footer -->
+
+        <!-- /footer -->
+
+
+    </body>
+</html>
